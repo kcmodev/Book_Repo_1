97799aa7a6301b2e426cfdc3ed83c179ca58657e
kcmodev <shchristensen3@gmail.com>
2020-05-06 15:33:17 -0500

    added all classes and headers

--- Christensen_PA/degree.h
+++ Christensen_PA/degree.h
@@ -1,1 +1,10 @@
-enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
+#ifndef degree.h
+#define degree.h
+using namespace std;
+
+
+class DegreeProgram {
+    // enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
+};
+
+#endif
--- Christensen_PA/main.cpp
+++ Christensen_PA/main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <cstring>
+#include "degree.h"
+#include "roster.h"
+#include "student.h"
 using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    // const string STUDENT_DATA[] = {
+    //     "A1, John, Smith, John1989@gmail.com, 20, 30, 35, 40, SECURITY", 
+    //     "A2, Suzan, Erickson, Erickson_1990@gmailcom, 19, 50, 30, 40, NETWORK", 
+    //     "A3, Jack,Napoli, The_lawyer99yahoo.com, 19, 20, 40, 33, SOFTWARE", 
+    //     "A4, Erin, Black, Erin.black@comcast.net, 22, 50, 58, 40, SECURITY", 
+    //     "A5, Steven, Christensen, schr206@wgu.edu, 2, 5, 10, 15, SOFTWARE"
+    //     };
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    // for (const string& word : STUDENT_DATA)
+    // {
+    //     cout << word << " ";
+    //     cout << endl;
+    // }
+    // cout << endl;
 
     return 0;
 }
